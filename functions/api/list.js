6225679efe0b0d719ed854ce1f32719ba256d827
andrewzinkyaw <andrewzinkyaw@gmail.com>
2026-07-07 17:30:34 +0630

    Update list.js

--- functions/api/list.js
+++ functions/api/list.js
@@ -1,9 +1,28 @@
-export async function onRequestGet(context) {
-  const { env } = context;
+export async function onRequestGet({ env }) {
 
-  const { results } = await env.DB.prepare(
-    "SELECT * FROM vless_keys ORDER BY id DESC"
-  ).all();
+    try {
 
-  return Response.json(results);
+        const { results } = await env.DB.prepare(`
+            SELECT
+                id,
+                title,
+                country,
+                type,
+                config,
+                is_premium
+            FROM vless_keys
+            ORDER BY id DESC
+        `).all();
+
+        return Response.json(results);
+
+    } catch (err) {
+
+        return Response.json({
+            success: false,
+            message: err.message
+        });
+
+    }
+
 }
