ca5ecee74f92ae8afa04e9d11ee7ef75b68f1ad0
andrewzinkyaw <andrewzinkyaw@gmail.com>
2026-07-07 12:24:36 +0630

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,32 @@
 const btn = document.getElementById("loginBtn");
 
 btn.onclick = async () => {
-  const password = document.getElementById("password").value;
+  try {
+    const password = document.getElementById("password").value;
 
-  const res = await fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      password: password
-    })
-  });
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        password: password
+      })
+    });
 
-  const data = await res.json();
-alert(JSON.stringify(data));
-  
-  if (data.success) {
-    localStorage.setItem("admin", "true");
-    document.body.innerHTML =
-"<pre>" + JSON.stringify(data, null, 2) + "</pre>";
-  } else {
-    document.getElementById("msg").innerText = data.message;
+    const data = await res.json();
+
+    alert(JSON.stringify(data));
+
+    if (data.success) {
+      localStorage.setItem("admin", "true");
+      alert("LOGIN SUCCESS");
+      window.location.href = "/ms-admin/dashboard.html";
+    } else {
+      alert(data.message);
+      document.getElementById("msg").innerText = data.message;
+    }
+  } catch (e) {
+    alert(e.toString());
   }
 };
