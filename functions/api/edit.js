9e43ede0d7d22aba7255c977ed6b3ff0fee1d75f
andrewzinkyaw <andrewzinkyaw@gmail.com>
2026-07-07 13:55:20 +0630

    Update edit.js

--- functions/api/edit.js
+++ functions/api/edit.js
@@ -1,19 +1,41 @@
 export async function onRequestPost(context) {
-  const { request, env } = context;
 
-  const { id, title, country, type, config, is_premium } =
-    await request.json();
+    const { request, env } = context;
 
-  await env.DB.prepare(`
-    UPDATE vless_keys
-    SET title=?, country=?, type=?, config=?, is_premium=?
-    WHERE id=?
-  `)
-  .bind(title, country, type, config, is_premium, id)
-  .run();
+    const body = await request.json();
 
-  return Response.json({
-    success: true,
-    message: "Key Updated!"
-  });
+    const {
+        id,
+        title,
+        country,
+        type,
+        config,
+        is_premium
+    } = body;
+
+    await env.DB.prepare(`
+        UPDATE vless_keys
+        SET
+            title = ?,
+            country = ?,
+            type = ?,
+            config = ?,
+            is_premium = ?
+        WHERE id = ?
+    `)
+    .bind(
+        title,
+        country,
+        type,
+        config,
+        is_premium,
+        id
+    )
+    .run();
+
+    return Response.json({
+        success: true,
+        message: "Key Updated Successfully!"
+    });
+
 }
