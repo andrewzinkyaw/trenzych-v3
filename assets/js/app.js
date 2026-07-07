b0c2bec85f506abc0ecf17626dba9bce40fc8175
andrewzinkyaw <andrewzinkyaw@gmail.com>
2026-07-07 12:13:18 +0630

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,12 @@
-alert("APP JS LOADED");
-
 const btn = document.getElementById("loginBtn");
 
 btn.onclick = async () => {
-
   const password = document.getElementById("password").value;
 
   const res = await fetch("/api/login", {
     method: "POST",
     headers: {
-      "Content-Type":"application/json"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
       password: password
@@ -18,16 +15,10 @@ btn.onclick = async () => {
 
   const data = await res.json();
 
-  if(data.success){
-
-    localStorage.setItem("admin","1");
-
-    location.href="/ms-admin/dashboard.html";
-
-  }else{
-
-    document.getElementById("msg").innerText=data.message;
-
+  if (data.success) {
+    localStorage.setItem("admin", "true");
+    location.href = "/ms-admin/dashboard.html";
+  } else {
+    document.getElementById("msg").innerText = data.message;
   }
-
 };
