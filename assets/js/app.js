5c87b8dd8435705b424ab7b1ef2e039de28bc1c3
andrewzinkyaw <andrewzinkyaw@gmail.com>
2026-07-07 11:45:45 +0630

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-// TRENZYCH VPN v3
+const btn = document.getElementById("loginBtn");
+
+btn.onclick = async () => {
+
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      password: password
+    })
+  });
+
+  const data = await res.json();
+
+  if(data.success){
+
+    localStorage.setItem("admin","1");
+
+    location.href="/ms-admin/dashboard.html";
+
+  }else{
+
+    document.getElementById("msg").innerText=data.message;
+
+  }
+
+};
